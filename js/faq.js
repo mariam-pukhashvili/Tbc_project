f5a68180d03d0679078b42024d020737ee52b099
mariam-pukhashvili <36764834+mariam-pukhashvili@users.noreply.github.com>
2024-01-27 22:00:57 +0400

    message

--- js/faq.js
+++ js/faq.js
@@ -1,5 +1,14 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((element) => {
-	console.log(element.nextSibling);
+	element.addEventListener("click", function (event) {
+		let answer = this.nextElementSibling;
+		if (answer.style.display === "block") {
+			answer.style.display = "none";
+			this.querySelector(".img img").style.transform = "rotate(0deg)";
+		} else {
+			answer.style.display = "block";
+			this.querySelector(".img img").style.transform = "rotate(180deg)";
+		}
+	});
 });
